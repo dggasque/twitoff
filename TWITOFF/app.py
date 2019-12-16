56595d40c52c8c8c24834ff60f5ce6781143ad72
dggasque <dggasque@users.noreply.guthub.com>
2019-12-16 14:00:15 -0800

    add TWITOFF app and template files

---
+++ TWITOFF/app.py
@@ -0,0 +1,20 @@
+"""Code for our app"""
+
+from flask import Flask
+from .models import DB
+
+# make our app factory
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # haver the database know about app
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        return 'Welcome to Twitoff!!'
+    return app 
---
+++ hello.py
@@ -0,0 +1,21 @@
+"""Minimal flask app"""
+
+from flask import Flask, render_template
+
+
+#Make the application
+app = Flask(__name__)
+
+#Make the route
+@app.route("/")
+
+#Now define a function
+def hello():
+    return render_template('home.html')
+
+#Make a second route
+@app.route("/about")
+
+#Now make the function that goes with about
+def preds():
+    return render_template('about.html')
