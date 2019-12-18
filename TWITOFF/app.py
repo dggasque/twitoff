53e70eb9fd7eca7ede85027b386835cafa8ba20a
dggasque <dggasque@users.noreply.guthub.com>
2019-12-18 12:27:14 -0800

    updates to py files and templates with more functionality adds predict.py prediction.html and user.html

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -4,29 +4,63 @@ from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
 
+from .twitter import add_or_update_user
+from .predict import predict_user
 
-# make our app factory
+from dotenv import load_dotenv
+load_dotenv()
+
+#make our app factory
+
 def create_app():
     app = Flask(__name__)
 
-    # add config for database
+    #add config for database
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 
-    # 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   
-    
-    # have the database know about app
+    #stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
         users = User.query.all()
-        return render_template('base.html', title='Home', users=users)
-    
+        return render_template('base.html', title = 'Home', users=users)
+
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title='Reset', users=[])
+        return render_template('base.html', title = 'Reset', users=[])
 
-    return app 
+    @app.route('/user',methods=['POST'])
+    @app.route('/user/<name>',methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+        message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+            request.values['tweet_text'], user1 if prediction else user2,
+            user2 if prediction else user1
+            )
+        return render_template('prediction.html', title='Prediction', message=message)
+    return app
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -2,16 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
-
 class User(DB.Model):
     """Twitter users that we analyze"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
     newest_tweet_id = DB.Column(DB.BigInteger)
-
     def __repr__(self):
         return '<User {}>'.format(self.name)
-
 
 class Tweet(DB.Model):
     """Tweets we pull"""
@@ -19,8 +16,9 @@ class Tweet(DB.Model):
     text = DB.Column(DB.Unicode(300))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
-    
+
     embedding = DB.Column(DB.PickleType, nullable=False)
 
     def __repr__(self):
         return '<Tweet {}>'.format(self.text)
+        
